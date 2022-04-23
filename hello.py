c38d63d845de0ab2f8bd05eaa98cec35b94173cc
kdforgive <102434641+kdforgive@users.noreply.github.com>
2022-04-23 11:19:14 +0300

    merge1 (#4)

--- hello.py
+++ hello.py
@@ -1,15 +1,4 @@
-d = 10
-
-
-def lol():
-    a = 10
-    return a
-
-
-print('h+++++el-----')
-print('wor++++ld')
-x = 1
-
-print('feature1 234 ')
-a, b = 1, 2
-
+print('hello')
+print('world')
+print('feature1')
+print("i'm adequate man")
